d5413baaad91835cf0a7043d47a97b4034db3fe2
Shivangi Verma <vhshlsimmi@gmail.com>
2026-06-20 00:36:40 +0800

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,40 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if(len(prices)==1):
+            return 0
+        max=prices[1]-prices[0]
+        for i in range(len(prices)-1):#0-4
+            #max=prices[i+1]-prices[i]#-6,4,-2
+            print ('out', max)
+            for j in range(i+1,len(prices)):
+                
+                profit=prices[j]-prices[i]
+                if profit>max:
+                        max=profit#5
+                print ('in',profit, max)
+
+        if max<0: 
+            return 0
+        else:
+            return max
+                    
+
+
+
+
+
+    # def find (self, profits: List[int]) -> int:
+    #    max=prices[0]
+    #    for i in range(len(profits)):
+    #     profit = profits[i]
+    #     if profit>max:
+    #         max=profit
+    #     else:
+    #         continue:
+
+    #     return max
+
+
+
+
+                
